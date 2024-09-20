572a94b0e35bfe1ee99001dbd7f378f778a2ec46
xxx1703xxx <119849514+xxx1703xxx@users.noreply.github.com>
2024-09-20 09:20:20 +0300

    Add files via upload

---
+++ Company.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1333333.Models
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public string Discription { get; set; }
+        public  string Adress { get; set; }
+        public string Email { get; set; }
+        public int NamberGroup { get; set; }
+    }
+}
---
+++ ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication1333333.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
