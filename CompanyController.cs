54cc771a9a76995b3359f8ef18b5108b78e71334
xxx1703xxx <119849514+xxx1703xxx@users.noreply.github.com>
2024-09-20 09:19:58 +0300

    Add files via upload

---
+++ CompanyController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1333333.Models;
+
+namespace WebApplication1333333.Controllers
+{
+    public class CompanyController : Controller
+    {
+        private static readonly Random _random = new Random();
+        public IActionResult Index()
+        {
+            int randomNumber = _random.Next(0, 101);
+            ViewBag.RandomNumber = randomNumber;
+            return View();
+        }
+        //private static List<Company> companies = new List<Company>
+        //{
+        //    new Company { Name = "Company A", Discription = "Description A", Adress = "Address A", Email = "emailA@example.com", NamberGroup = 1 },
+        //    new Company { Name = "Company B", Discription = "Description B", Adress = "Address B", Email = "emailB@example.com", NamberGroup = 2 }
+        //};
+    }
+}
